e9ac91b8718d8d97b849512a960ab5ff958e2d15
Olina Stathopoulou <olina.stath@nyu.edu>
2020-06-06 03:06:17 +0100

    fixes bc i messed up lol

--- webpack/base.js
+++ webpack/base.js
@@ -32,9 +32,6 @@ module.exports = {
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
     })
   ]
 };
