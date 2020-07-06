e35c509353ba88b747054a050a4ce6357d6ac5a7
Brandon <bsu24@bloomberg.net>
2020-07-05 17:13:36 -0700

    new game

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -139,7 +139,7 @@ export default class Game extends Phaser.Scene {
     // SOCKET STUFF
     // env vars don't get passed in from server.js
 
-    process.env.SERVER = "https://03387c66396b.ngrok.io";
+    process.env.SERVER = "http://8d84bfcd469a.ngrok.io";
 
     this.socket = io(process.env.SERVER || "http://localhost:2000/");
     this.socket.on("connect", function () {
